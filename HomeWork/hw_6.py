bd9e6c15eb9a783f365b2bbc0039cf4b582dd089
Argen <_bola2004bol@gmail.com>
2025-11-17 20:02:18 +0300

    hw_6

---
+++ HomeWork/hw_6.py
@@ -0,0 +1,47 @@
+"""Импорт и настройка:"""
+from faker import Faker
+
+"""Указываем локлизацию"""
+fake = Faker("ru_RU")
+
+
+"""Генерация нескольких записей"""
+# for _ in range(5):
+#     print({
+#         "name": fake.name(),
+#         "email":fake.email(),
+#         "address":fake.address().replace("\n",","),
+#         "lat": float(fake.latitude()),
+#         "lon": float(fake.longitude()),
+#         "website": fake.url()
+#     })
+
+"""Улучшенный вывод ткрминала"""
+for _ in range(5):
+    person = ({
+        "name": fake.name(),
+        "email":fake.email(),
+        "address":fake.address().replace("\n",","),
+        "lat": float(fake.latitude()),
+        "lon": float(fake.longitude()),
+        "website": fake.url()
+    })
+
+    print("────────────────────────────────────────")
+    print(f"Имя:     {person['name']}")
+    print(f"Email:   {person['email']}")
+    print(f"Адрес:   {person['address']}")
+    print(f"Широта:  {person['lat']}")
+    print(f"Долгота: {person['lon']}")
+    print(f"Сайт:    {person['website']}")
+    print("────────────────────────────────────────\n")
+
+
+
+
+"""Генерация базовых данных"""
+# print(fake.name())
+# print(fake.address().replace("\n", ","))
+# print(fake.text(max_nb_chars=200))
+# print(fake.email())
+# print(fake.country())
